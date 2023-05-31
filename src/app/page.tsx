3935fe7e92128942883d9383db2b17c0dececa04
Allaff Gustavo <allaff.ribeiro@academico.ifpb.edu.br>
2023-05-31 10:47:21 -0300

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 export default function Home() {
-    return (
-        <><h1>Olá</h1><h3>oi</h3></>
-    )
+  return (
+    <>
+      <h1>Teste 1</h1>
+      <p>Testando do meu para o remoto</p>
+      <h3>oi</h3>
+    </>
+  )
 }
